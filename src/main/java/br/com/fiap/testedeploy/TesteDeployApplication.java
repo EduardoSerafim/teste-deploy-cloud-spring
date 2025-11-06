dde4d54542d1b88add2d7fa9c6cd9743c29e88c3
Eduardo Serafim <eduserafim7@gmail.com>
2025-11-05 22:50:17 -0300

    projeto criado

---
+++ src/main/java/br/com/fiap/testedeploy/TesteDeployApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.testedeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/testedeploy/TesteDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.testedeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
